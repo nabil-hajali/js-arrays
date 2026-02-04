1fd334d38e2bae67afa9a55883747ce02278feb2
nabil-hajali <nabil2491@gmail.com>
2026-02-04 16:58:23 +0100

    remove first teacher name

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,7 +29,10 @@ console.log(teachers);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = 'Nathan';
+teachers.shift()
+console.log(teachers);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
